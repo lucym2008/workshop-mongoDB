96d18f1b95d6dfc541ebf4f07817f0377710373d
lucym2008 <leonardomachadoarroyo14@gmail.com>
2026-03-14 22:04:47 -0300

    add the connection with database

--- src/main/java/com/leonardomachado/workshop_mongo/resource/UserResource.java
+++ src/main/java/com/leonardomachado/workshop_mongo/resource/UserResource.java
@@ -1,27 +1,26 @@
 package com.leonardomachado.workshop_mongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonardomachado.workshop_mongo.entities.User;
+import com.leonardomachado.workshop_mongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService serv;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Brown", "mariabrown@gmail.com");
-		User alex = new User("1", "Alex Green", "alexgreen@gmail.com");
-		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = serv.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
